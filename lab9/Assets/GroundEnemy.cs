c7912c649ca91924f96a1058d3907bba1c261945
StrangeriX <mqteuszgrzesik@gmail.com>
2020-12-16 15:32:13 +0100

    final commit for game, lab9

---
+++ lab9/Assets/GroundEnemy.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundEnemy : MonoBehaviour
+{
+    float speed = 0.5f;
+
+    Vector3 moveTo;
+    public  GameObject pointA;
+    public  GameObject pointB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+        moveTo = pointA.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector3.Lerp(transform.position, moveTo, speed*Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+        if(transform.position.x >= pointA.transform.position.x-0.5f)
+        {
+            moveTo = pointB.transform.position;
+            transform.eulerAngles = new Vector3(0, -180, 0);
+        }
+        else if(transform.position.x <= pointB.transform.position.x+0.5f)
+        {
+            moveTo = pointA.transform.position;
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+            
+    }
+}
---
+++ lab9/Assets/Scripts/Patrol.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    private float speed = 2f;
+    public float distance;
+
+    private bool movingRight = true;
+    Vector2 lookAt = Vector2.right;
+
+    public Transform groundDetection;
+    public Transform sightDetection;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        RaycastHit2D sightInfo = Physics2D.Raycast(sightDetection.position, lookAt, 10f);
+
+        Debug.Log(lookAt);
+
+        if (groundInfo.collider == false)
+        {
+            Debug.Log("turn");
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+                lookAt = Vector2.left;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+                lookAt = Vector2.right;
+            }
+        }
+
+        if (sightInfo == true)
+        {
+            speed = 5f;
+        }
+        else
+        {
+            speed = 2f;
+        }
+    }
+}
